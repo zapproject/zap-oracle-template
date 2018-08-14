8523c2723473d11fac0a0bdd6617b05b5a9c9e15
Michael Man <17michaelm@gmail.com>
2018-08-14 13:41:04 -0400

    Fixed minor bugs

--- src/helper.ts
+++ src/helper.ts
@@ -6,7 +6,7 @@ const Web3 = require('web3');
 const request = require('request');
 
 import { ZapProvider } from "@zapjs/provider";
-import { getWeb3Provider, Responders, ProviderData } from "./provider";
+import { getWeb3Provider, Responders, ProviderData } from "./oracle";
 import { join } from "path";
 
 //==============================================================================================================
@@ -82,7 +82,7 @@ export type ZapResponder = {
  	// loads the first account for this web3 provider
  	const accounts: string[] = await web3.eth.getAccounts();
  	if ( accounts.length == 0 ) throw('Unable to find an account in the current web3 provider');
-
+ 	const owner: string = accounts[0];
  	console.log("Loaded account:", owner);
  	// TODO: Add Zap balance
  	console.log("Wallet contains:", await web3.eth.getBalance(owner) / 1e18, "ETH");

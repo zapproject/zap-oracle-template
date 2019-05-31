4791163871af80117b9ca0eb8da4835736f30535
minciong <maxraphael_inciong@yahoo.com>
2019-05-31 12:43:02 -0400

    got rid of annoying syntax bug

--- Oracle/Responder.ts
+++ Oracle/Responder.ts
@@ -1,5 +1,5 @@
 
-export async function getResponse(query:string,params?:string[]|[]){
+export async function getResponse(query:string,params:string[]|[]){
 	//Get data based on the query string and Parameters
 		return ['response here']
 

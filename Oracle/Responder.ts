fa0d912804055ef56a64dfc7ba7f16ab27c3f3b4
minciong <maxraphael_inciong@yahoo.com>
2019-07-17 17:47:52 -0400

    sums added using cloc

--- Oracle/Responder.ts
+++ Oracle/Responder.ts
@@ -1,5 +1,31 @@
 const request = require('request');
 const web3=require('web3')
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const tmp = require('tmp');
+
+
+async function getCLOC(repository:any) {
+    const tmpobj = tmp.dirSync();
+    const { _, git_err } = await exec("git clone " + repository + " " + tmpobj.name);
+
+    if ( git_err ) {
+        console.warn("Git Error:", git_err);
+    }
+
+    const { stdout, stderr } = await exec("cloc --json --git " + tmpobj.name);
+
+    if ( stderr ) {
+        throw new Error("Failed to run cloc: " + stderr.toString());
+    }
+
+    // console.log(JSON.parse(stdout));
+    // console.log(JSON.parse(stdout)["SUM"]["code"]);
+    tmpobj.removeCallback();
+    return JSON.parse(stdout)["SUM"]["code"];
+}
+
+
 function requestPromise(url:string, method:any = "GET", headers:any = -1, data:any = -1) {
     var trans:any = {
         method: method,
@@ -26,33 +52,21 @@ export async function getResponse(query:string,params:string[]|[]){
 	//Get data based on the query string and Parameters
 
 		 try  {
-        var timestamp:any = parseInt(params[0])//Date.parse(params[2])/1000
-        // var timestamp:any = Date.parse('2019-01-01T03:24:00')/1000
-        console.log(timestamp)
-        var commitCount:number=0;
-        var mostCommits:number = 0;
+        
+        var count:number=0;
+        var most:number = 0;
         var winnerIndex:number=1;
-        for(var p = 1;p<params.length;p++){
-        var url:string = "https://api.github.com/repos/"+params[p]+"/stats/contributors"//?w="+timestamp;
-        
+        for(var p = 0;p<params.length;p++){
+        var url:string = "https://github.com/"+params[p]
+        count =  await getCLOC(url);
         console.log(url)
-        var agent:string = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36';
-        const body:any = await requestPromise(url,null,{'User-Agent': agent});
-        const json:any = JSON.parse(body);
-
-        commitCount = 0;
-        for(var i=0;i<json.length;i++){
-            for(var j = json[i]["weeks"].length-1;j>0;j--){
-                if(json[i]["weeks"][j]["w"]<timestamp)break;
-                commitCount+=json[i]["weeks"][j]["c"]
-            }
         
-        }
-        console.log(commitCount)
-        if(commitCount>mostCommits){
-            mostCommits=commitCount
+        console.log(count)
+        if(count>most){
+            most=count
             winnerIndex=p
         }
+        
         }
         console.log(web3.utils.asciiToHex(params[winnerIndex]))
         return [web3.utils.asciiToHex(params[winnerIndex])]
@@ -64,3 +78,4 @@ export async function getResponse(query:string,params:string[]|[]){
 
 }
 
+var x = getResponse("",["zapproject/zap-ethereum-api","zapproject/zap-oracle-template"])

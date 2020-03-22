5cfe8dcb1e42c6614c31e3d35b1c858c8da35fb0
minciong <maxraphael_inciong@yahoo.com>
2020-03-22 16:40:02 -0400

    simple price query coingecko

--- Oracle/Responder.ts
+++ Oracle/Responder.ts
@@ -1,6 +1,19 @@
+const CoinGecko = require('coingecko-api');
+const CoinGeckoClient = new CoinGecko();
 
 export async function getResponse(query:string,params:string[]|[]){
 	//Get data based on the query string and Parameters
-		return ['response here']
+	let data:any = await CoinGeckoClient.simple.price({
+    ids: [query],
+    vs_currencies: params,
+});
+	var arr:any = [];
+	for (var key in data["data"][query]){
+		arr.push(Math.floor(data["data"][query][key]*100));
+	}
+	// console.log(arr);	
+		return arr;
 
 }
+
+// var func = getResponse("bitcoin",["eur","usd"])

ab4f0ea753024fc67b7b22f89f99c7d99d688a24
Kim <propulsor.ichimoku@gmail.com>
2020-12-25 13:25:55 -0700

    Upgraded dependencies, defined node engine versions (#8)
    Co-authored-by: kim <kim@kims-MacBook-Pro.local>

--- Oracle/Oracle.ts
+++ Oracle/Oracle.ts
@@ -1,22 +1,25 @@
-import {getResponse} from "./Responder"
+import { getResponse } from "./Responder"
 import * as Config from "./Config.json"
 const Web3 = require('web3');
 import { ZapProvider } from "@zapjs/provider";
-import {ZapToken} from "@zapjs/zaptoken"
-const HDWalletProviderMem = require("truffle-hdwallet-provider");
-const {toBN,fromWei, hexToUtf8} =require("web3-utils");
+import { ZapToken } from "@zapjs/zaptoken"
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { toBN, fromWei, hexToUtf8 } = require("web3-utils");
 const assert = require("assert")
 const IPFS = require("ipfs-mini")
-const ipfs = new IPFS({host:'ipfs.infura.io',port:5001,protocol:'https'})
+const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' })
 const IPFS_GATEWAY = "https://gateway.ipfs.io/ipfs/"
-import {connectStatus} from "./Status"
+import { connectStatus } from "./Status"
 
-export  class ZapOracle {
-    web3:any
-    oracle:any
-    zapToken:any
-    constructor(){
-        this.web3 = new Web3(new HDWalletProviderMem(Config.mnemonic, Config.NODE_URL))
+export class ZapOracle {
+    web3: any
+    oracle: any
+    zapToken: any
+    constructor() {
+        const wsProvider = new Web3.providers.WebsocketProvider(Config.NODE_URL)
+        HDWalletProvider.prototype.on = wsProvider.on.bind(wsProvider)
+        const provider = new HDWalletProvider(Config.mnemonic, wsProvider)
+        this.web3 = new Web3(provider)
         this.oracle = null
         this.zapToken = null
     }
@@ -43,18 +46,18 @@ export  class ZapOracle {
         await this.getProvider();
         await this.delay(5000)
         const title = await this.oracle.getTitle();
-        console.log(title)
+        console.log("Provider's title: ", title)
         if (title.length == 0) {
             console.log("No provider found, Initializing provider");
-            const res: string = await this.oracle.initiateProvider({title: Config.title, public_key: Config.public_key});
+            const res: string = await this.oracle.initiateProvider({ title: Config.title, public_key: Config.public_key });
             console.log("Successfully created oracle", Config.title);
         }
         else {
             console.log("Oracle exists");
-            if( title != Config.title){
-              console.log("Changing title")
-              const res:string = await this.oracle.setTitle({title:Config.title})
-              console.log("Successfully changed Title : ",res)
+            if (title != Config.title) {
+                console.log("Changing title")
+                const res: string = await this.oracle.setTitle({ title: Config.title })
+                console.log("Successfully changed Title : ", res)
 
             }
         }
@@ -64,60 +67,60 @@ export  class ZapOracle {
         if (!curveSet) {
             //create endpoint
             console.log("No matching Endpoint found, creating endpoint")
-            if(endpoint.broker == "")
+            if (endpoint.broker == "")
                 endpoint.broker = "0x0000000000000000000000000000000000000000"
-            const createEndpoint = await this.oracle.initiateProviderCurve({endpoint: endpoint.name, term: endpoint.curve, broker: endpoint.broker});
+            const createEndpoint = await this.oracle.initiateProviderCurve({ endpoint: endpoint.name, term: endpoint.curve, broker: endpoint.broker });
             console.log("Successfully created endpoint ", createEndpoint)
             //setting endpoint params with indexed query string
-            let endpointParams:string[] = []
-            for(let query of endpoint.queryList){
-                endpointParams.push("Query string :"+ query.query +", Query params :"+JSON.stringify(query.params)+", Response Type: "+query.responseType)
+            let endpointParams: string[] = []
+            for (let query of endpoint.queryList) {
+                endpointParams.push("Query string :" + query.query + ", Query params :" + JSON.stringify(query.params) + ", Response Type: " + query.responseType)
             }
             console.log("Setting endpoint params")
 
-            const txid = await this.oracle.setEndpointParams({endpoint: endpoint.name, endpoint_params: endpointParams})
+            const txid = await this.oracle.setEndpointParams({ endpoint: endpoint.name, endpoint_params: endpointParams })
             console.log(txid)
             // setting {endpoint.json} file and save it to ipfs
-            let ipfs_endpoint:any = {}
-            ipfs_endpoint.name =  endpoint.name
+            let ipfs_endpoint: any = {}
+            ipfs_endpoint.name = endpoint.name
             ipfs_endpoint.curve = endpoint.curve
             ipfs_endpoint.broker = endpoint.broker
             ipfs_endpoint.params = endpointParams
             // add to ipfs file
             console.log("Saving Endpoint info into ipfs")
-            ipfs.addJSON(ipfs_endpoint,(err:any,res:any)=>{
-                if(err){
+            ipfs.addJSON(ipfs_endpoint, (err: any, res: any) => {
+                if (err) {
                     console.error("Fail to save endpoint data to ipfs : ", ipfs_endpoint)
                     process.exit(err)
                 }
                 //save ipfs hash to this.oracle param
                 console.log("Successfully saved Endpoint json file into ipfs, saving ipfs link to oracle's params")
-                this.oracle.setProviderParameter({key:`${endpoint.name}.json`,value:IPFS_GATEWAY+res})
-                    .then((txid:string)=>{console.log("saved endpoint info to param with hash : ",res,txid)})
+                this.oracle.setProviderParameter({ key: `${endpoint.name}.json`, value: IPFS_GATEWAY + res })
+                    .then((txid: string) => { console.log("saved endpoint info to param with hash : ", res, txid) })
             })
             //if there is a md string => save to provider params
-            if(endpoint.md && endpoint.md!=""){
-                ipfs.add(endpoint.md,(err:any,res:any)=>{
-                  if(err){
-                      console.error("Fail to save endpoint .md file to ipfs", endpoint)
-                      process.exit(err)
-                  }
-                  //set ipfs hash as provider param
-                  this.oracle.setProviderParameter({key:`${endpoint.name}.md`,value:IPFS_GATEWAY+res})
-                    .then((txid:string)=>{console.log("saved endpoint info to param with hash : ",res,txid)})
+            if (endpoint.md && endpoint.md != "") {
+                ipfs.add(endpoint.md, (err: any, res: any) => {
+                    if (err) {
+                        console.error("Fail to save endpoint .md file to ipfs", endpoint)
+                        process.exit(err)
+                    }
+                    //set ipfs hash as provider param
+                    this.oracle.setProviderParameter({ key: `${endpoint.name}.md`, value: IPFS_GATEWAY + res })
+                        .then((txid: string) => { console.log("saved endpoint info to param with hash : ", res, txid) })
 
                 })
             }
-            else{
-              console.log("No md value file, skipping")
+            else {
+                console.log("No md value file, skipping")
             }
         }
         else {
-          //Endpoint is initialized, so ignore all the setup part and listen to Query
+            //Endpoint is initialized, so ignore all the setup part and listen to Query
             console.log("curve is already  set : ", await this.oracle.getCurve(endpoint.name))
         }
         //UPDATE STATUS TO ZAP
-        connectStatus(this.web3,endpoint)
+        connectStatus(this.web3, endpoint)
         console.log("Start listening and responding to queries")
         this.oracle.listenQueries({}, (err: any, event: any) => {
             if (err) {
@@ -134,28 +137,28 @@ export  class ZapOracle {
      * @returns ZapProvider instantiated
      */
 
-    delay = (ms:number) => new Promise(_ => setTimeout(_, ms));
+    delay = (ms: number) => new Promise(_ => setTimeout(_, ms));
     async getProvider() {
         // loads the first account for this web3 provider
         const accounts: string[] = await this.web3.eth.getAccounts();
-        if (accounts.length == 0) throw('Unable to find an account in the current web3 provider, check your Config variables');
+        if (accounts.length == 0) throw ('Unable to find an account in the current web3 provider, check your Config variables');
         const owner: string = accounts[0];
         this.oracle = new ZapProvider(owner, {
             networkId: (await this.web3.eth.net.getId()).toString(),
-            networkProvider:this.web3.currentProvider
+            networkProvider: this.web3.currentProvider
         });
         this.zapToken = new ZapToken({
             networkId: (await this.web3.eth.net.getId()).toString(),
-            networkProvider:this.web3.currentProvider
+            networkProvider: this.web3.currentProvider
         })
         const ethBalance = await this.web3.eth.getBalance(owner)
         const zapBalance = await this.zapToken.balanceOf(owner)
-        console.log("Wallet contains:", fromWei(ethBalance,"ether"),"ETH ;", fromWei(zapBalance,"ether"),"ZAP");
+        console.log("Wallet contains:", fromWei(ethBalance, "ether"), "ETH ;", fromWei(zapBalance, "ether"), "ZAP");
     }
 
-//==============================================================================================================
-// Query Handler
-//==============================================================================================================
+    //==============================================================================================================
+    // Query Handler
+    //==============================================================================================================
 
     /**
      * Handles a query
@@ -186,22 +189,22 @@ export  class ZapOracle {
         console.log(`Received query to ${event.endpoint} from ${event.onchainSub ? 'contract' : 'offchain subscriber'} at address ${event.subscriber}`);
         console.log(`Query ID ${event.queryId.substring(0, 8)}...: "${event.query}". Parameters: ${event.endpointParams}`);
         for (let query of Config.EndpointSchema.queryList) {
-            try{
-              // Call the responder callback to get the data needed for this query
-              let response = await getResponse(event.query,event.endpointParams)
-              console.log("got response from getResponse method : ", response)
+            try {
+                // Call the responder callback to get the data needed for this query
+                let response = await getResponse(event.query, event.endpointParams)
+                console.log("got response from getResponse method : ", response)
 
-              // Send the response
-              console.log("Responding to offchain subscriber : ")
+                // Send the response
+                console.log("Responding to offchain subscriber : ")
                 let txid = await this.oracle.respond({
                     queryId: event.queryId,
                     responseParams: response,
                     dynamic: query.dynamic
                 })
 
-              console.log('Responded to', event.subscriber, "in transaction", txid.transactionHash);
-            }catch(e){
-              throw new Error(`Error responding to query ${event.queryId} : ${e}`)
+                console.log('Responded to', event.subscriber, "in transaction", txid.transactionHash);
+            } catch (e) {
+                throw new Error(`Error responding to query ${event.queryId} : ${e}`)
             }
         }
     }
--- Oracle/Status.ts
+++ Oracle/Status.ts
@@ -2,61 +2,56 @@
   Call zap server to let it knows oracle is still online
 */
 import * as Config from "./Config.json"
-const Web3 = require('web3');
-import {toHex,utf8ToHex} from "web3-utils"
-import {ZapProvider} from "@zapjs/provider"
-import {ZapBondage} from "@zapjs/bondage"
-const HDWalletProviderMem = require("truffle-hdwallet-provider");
-const io =require("socket.io-client");
+const io = require("socket.io-client");
 
-const rq = require("request-promise")
+const axios = require("axios")
 const ZAP_SERVER = "http://localhost:8000"
 
-export const updateStatus = function(web3:any,oracle:any,endpoint:any){
-  update(web3,oracle,endpoint)
-  setInterval(()=>{
-    update(web3,oracle,endpoint)
-  },3*60*1000) //every  minutes
+export const updateStatus = function (web3: any, oracle: any, endpoint: any) {
+  update(web3, oracle, endpoint)
+  setInterval(() => {
+    update(web3, oracle, endpoint)
+  }, 3 * 60 * 1000) //every  minutes
 }
 
-async function update(web3:any,oracle:any,endpoint:string){
+async function update(web3: any, oracle: any, endpoint: string) {
   console.log("update status")
-  try{
+  try {
     let time = new Date().getTime()
-    const data = endpoint + ":"+time
+    const data = endpoint + ":" + time
     console.log(data)
-    let signature = await web3.eth.sign(data,oracle)
+    let signature = await web3.eth.sign(data, oracle)
     let options = {
-      method:"POST",
-      uri:ZAP_SERVER+"/update",
-      body:{
+      method: "POST",
+      uri: ZAP_SERVER + "/update",
+      body: {
         data,
         signature
       },
-      json:true
+      json: true
     }
-    const result = await rq(options)
+    const result = await axios(options)
     console.log(result)
-  }catch(e){
+  } catch (e) {
     console.error(e)
   }
 }
 
 
-export const connectStatus= async (web3:any,endpoint:any)=>{
+export const connectStatus = async (web3: any, endpoint: any) => {
   let accounts = await web3.eth.getAccounts()
   let oracle = accounts[0]
   console.log(oracle)
-  let socket = io(Config.STATUS_URL,{path:"/ws/",secure:true})
-  socket.on("connect",async ()=>{
-    const signature = await web3.eth.sign(endpoint,oracle)
+  let socket = io(Config.STATUS_URL, { path: "/ws/", secure: true })
+  socket.on("connect", async () => {
+    const signature = await web3.eth.sign(endpoint, oracle)
     console.log(signature)
-    socket.emit("authentication",{endpoint:"TrendSignals",signature})
+    socket.emit("authentication", { endpoint: "TrendSignals", signature })
   })
-  socket.on("authenticated",()=>{
+  socket.on("authenticated", () => {
     console.log("authenticated")
   })
-  socket.on("unauthorized",()=>{
+  socket.on("unauthorized", () => {
     console.log("unauthorized")
   })
 }

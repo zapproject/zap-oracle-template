e7c56b8914ef4463d1bd9e59631161a74eef89b6
Harris Levine <acyclicgraphs@gmail.com>
2021-03-04 07:49:26 -0500

    Update Responder.ts

--- Oracle/Responder.ts
+++ Oracle/Responder.ts
@@ -1,33 +1,62 @@
-//1. Import coingecko-api
-const CoinGecko = require('coingecko-api');
-var Decimal = require('decimal.js');
-//2. Initiate the CoinGecko API Client
-const CoinGeckoClient = new CoinGecko();
+"use strict"
 
-//3. Make calls
-var func = async () => {
-        let data = await CoinGeckoClient.ping();
-};
+const axios = require('axios')
+const url = '/https://api-pub.bitfinex.com/v2/'
+
+const pathParams = 'ticker/tBTCUSD' // Change these based on relevant path params
+const queryParams = '' // Change these based on relevant query params
 
 export async function getResponse(coin: string, params?: string[] | []) {
         try {
-                let data = await CoinGeckoClient.coins.fetchMarketChart(coin.toLowerCase());
-                const price = data.data.prices[data.data.prices.length - 1][1]
-                console.log("data for coin", price[1])
-                return [price.toString()]
-        } catch (e) {
-                return [0] //return 0 to indicate price info is not available
+            const resp = await axios.request(`${url}/${pathParams}?${queryParams}`);
+            console.log("BTC/USD Price", `STATUS ${resp.status} - ${JSON.stringify(resp.data)}`);
         }
-}
-export async function getResponseInt(coin: string, params?: string[] | []) {
-        try {
-                let data = await CoinGeckoClient.coins.fetchMarketChart(coin.toLowerCase());
-                const price = data.data.prices[data.data.prices.length - 1][1]
-                console.log("data for coin", price[1])
-                let p=new Decimal(price.toString())
-                p=p.times('10e+18').floor()
-                return [p.toNumber()]
-        } catch (e) {
-                return [0] //return 0 to indicate price info is not available
+   catch (err) {
+       console.log(err)
         }
-}
+ }
+
+request()
+
+// export async function getResponseInt(coin: string, params?: string[] | []) {
+//         try {
+                
+//         } catch (e) {
+                
+//         }
+// }
+
+
+// //1. Import coingecko-api
+// const CoinGecko = require('coingecko-api');
+// var Decimal = require('decimal.js');
+// //2. Initiate the CoinGecko API Client
+// const CoinGeckoClient = new CoinGecko();
+
+// //3. Make calls
+// var func = async () => {
+//         let data = await CoinGeckoClient.ping();
+// };
+
+// export async function getResponse(coin: string, params?: string[] | []) {
+//         try {
+//                 let data = await CoinGeckoClient.coins.fetchMarketChart(coin.toLowerCase());
+//                 const price = data.data.prices[data.data.prices.length - 1][1]
+//                 console.log("data for coin", price[1])
+//                 return [price.toString()]
+//         } catch (e) {
+//                 return [0] //return 0 to indicate price info is not available
+//         }
+// }
+// export async function getResponseInt(coin: string, params?: string[] | []) {
+//         try {
+//                 let data = await CoinGeckoClient.coins.fetchMarketChart(coin.toLowerCase());
+//                 const price = data.data.prices[data.data.prices.length - 1][1]
+//                 console.log("data for coin", price[1])
+//                 let p=new Decimal(price.toString())
+//                 p=p.times('10e+18').floor()
+//                 return [p.toNumber()]
+//         } catch (e) {
+//                 return [0] //return 0 to indicate price info is not available
+//         }
+// }

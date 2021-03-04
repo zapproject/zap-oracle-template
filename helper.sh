6f4c52ba8f4bfc97b8132ef337469a85ad998527
blocksforked <76859972+blocksforked@users.noreply.github.com>
2021-03-04 11:42:48 -0500

    Update helper.sh
    update to fix

--- helper.sh
+++ helper.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 rm -r node_modules/@zapjs/artifacts/
 cp -r artifacts node_modules/@zapjs/artifacts/
-mkdir node_modules/@zapjs/artifacts/lib
-cp index.js node_modules/@zapjs/artifacts/lib/

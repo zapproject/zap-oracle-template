1a65751389847cc7a6cc19794f8c9336ce3dd563
John <jjj17@protonmail.com>
2021-03-24 11:44:14 -0400

    update responder to use int param

--- Oracle/Responder.ts
+++ Oracle/Responder.ts
@@ -14,20 +14,22 @@ export async function getResponse(coin: string, params?: string[] | []) {
                 let data = await CoinGeckoClient.coins.fetchMarketChart(coin.toLowerCase());
                 const price = data.data.prices[data.data.prices.length - 1][1]
                 console.log("data for coin", price[1])
-                return [price.toString()]
+                if(params){
+                        if(params[0]==='int'){
+                            let p=new Decimal(price.toString())
+                            p=p.times('10e+18').floor()
+                            return [p.toNumber()]
+                        }else{
+                            return [price.toString()]   
+                        }
+                       
+                }else{
+                        return [price.toString()]
+                }
+               
         } catch (e) {
                 return [0] //return 0 to indicate price info is not available
         }
 }
-export async function getResponseInt(coin: string, params?: string[] | []) {
-        try {
-                let data = await CoinGeckoClient.coins.fetchMarketChart(coin.toLowerCase());
-                const price = data.data.prices[data.data.prices.length - 1][1]
-                console.log("data for coin", price[1])
-                let p=new Decimal(price.toString())
-                p=p.times('10e+18').floor()
-                return [p.toNumber()]
-        } catch (e) {
-                return [0] //return 0 to indicate price info is not available
-        }
-}
+
+
